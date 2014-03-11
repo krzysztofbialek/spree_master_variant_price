9b8135d46ae3f931ba9bd944139ead825c92f8b6
krzysztofbialek <zlo@o2.pl>
2014-03-11 23:40:35 +0100

    make display price use master price as well

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -8,7 +8,15 @@ module Spree
       return product.master.price if use_master_price? && !is_master?
 
       default_price.price
+    end
 
+    def money
+      
+      if use_master_price? && !is_master?
+        Spree::Money.new(default_price.price || 0, { currency: currency })
+      else
+        Spree::Money.new(amount || 0, { currency: currency })
+      end
     end
 
   end
