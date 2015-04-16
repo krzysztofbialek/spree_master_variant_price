ff053229d1fa7b41700a2b5ad28ee2f3876a2446
krzysztofbialek <krzysztof.bialek@gmail.com>
2015-04-16 23:14:00 +0200

    overwrite price_in currency

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -7,18 +7,29 @@ module Spree
     def price
       return 0 unless self.persisted?
 
-      return product.master.price if use_master_price? && !is_master?
+      return product.master.price if with_master_price?
 
       default_price.price
     end
 
     def display_price
-      if use_master_price? && !is_master?
+      if with_master_price?
         Spree::Money.new(product.master.price || 0, { currency: currency })
       else
         Spree::Money.new(default_price.amount || 0, { currency: currency })
       end
     end
 
+    def price_in(currency)
+      if with_master_price?
+        product.master.prices.select{ |price| price.currency == currency }.first || Spree::Price.new(:variant_id => product.master.id, :currency => currency)
+      else  
+        prices.select{ |price| price.currency == currency }.first || Spree::Price.new(:variant_id => self.id, :currency => currency)
+      end
+    end
+
+    def with_master_price?
+      use_master_price? && !is_master?
+    end
   end
 end
